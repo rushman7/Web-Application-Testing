f74fcf03f6ef2574c7d8d5489477997c2c272fcf
BuildTools <arthurpisakhov1@gmail.com>
2019-09-29 19:28:47 -0400

    hit and foul fnc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const initial = {
+    balls: 0,
+    strikes: 0
+  }
+
+  const [count, setCount] = useState(initial);
+
+  const hit = () => {
+    setCount(initial);
+  }
+
+  const foul = () => {
+    if (count.strikes < 2) {
+      setCount(count.strikes++)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <span className="greet">Hello World!</span>
+        <span className="greet">Hello World</span>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,14 @@ import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-it('renders a span with the text Hello World', () => {
-  const wrapper = rtl.render(
-    <App />
-  );
-  console.log(wrapper.debug());
-});
+describe('App', () => {
+  it('renders a span with the text Hello World', () => {
+    const wrapper = rtl.render(<App />);
+    const element = wrapper.queryByText(/hello world/i);
+
+    console.log(element.textContent);
+    expect(element).toBeInTheDocument();
+    expect(element).toBeTruthy();
+    expect(element).toBeVisible();
+  })
+})
